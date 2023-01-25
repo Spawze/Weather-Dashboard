9bab84e10c231b26dc23a3c24b3368f860b7ea09
Riley Allred <riley_allred@hotmail.com>
2023-01-25 14:26:27 -0700

    finished all code and added picture asssets

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,50 @@
-//my weather api key = 6e36d909b1e27a85b3c299313b8d76b9
-
-//on init: pull searches from local storage and populate in history section
-
 const apiKey = "6e36d909b1e27a85b3c299313b8d76b9"
 var searchButtonEl = $('#search-button')
 var inputEl = $('#search')
 var searchOptionEl = $('#search-option')
+
 var searchTerm;
 var searchType;
-
 var searchCityName;
+
+var outputDivEl = $('#output-div')
 
 var mainCardCity = $('#searched-city')
 var mainCardDate = $('#main-date')
+var mainCardIcon = $('#main-icon')
+var mainCardTempSpan = $('#main-temp')
+var mainCardWindSpan = $('#main-wind')
+var mainCardHumiditySpan = $('#main-humidity')
 
 var cardDateEl = $('.date')
+var cardIconEl = $('.card-icon')
+var cardTempSpan = $('.card-temp')
+var cardWindSPan = $('.card-wind')
+var cardHumiditySpan = $('.card-humidity')
+
+var searchHistoryListEl = $('#search-history')
+var searchHistoryList = []
+
 
 //when search button is clicked take search text and make API call
-searchButtonEl.on('click', getLatAndLong)
+searchButtonEl.on('click', startSearchViaSearch)
 
-//is geo api to get latitude and longitude for the other search
-function getLatAndLong() {
-    var searchLat;
-    var searchLon;
-    
+function startSearchViaSearch() {
     searchTerm = inputEl.val()
     searchType = searchOptionEl.val()
+    getLatAndLong(searchTerm, searchType)
+}
+
+
+function startSearchViaHistory(id) {
+    searchTerm = searchHistoryList[id].term
+    searchType = searchHistoryList[id].type
+    getLatAndLong(searchTerm, searchType)
+}
+//is geo api to get latitude and longitude for the other search
+function getLatAndLong(searchTerm, searchType) {
+    var searchLat;
+    var searchLon;
 
     //only search if there is something in the search field
     if (searchTerm) {
@@ -63,45 +82,112 @@ function getLatAndLong() {
     }
 }
 
+//using lat and long fromm geo api we get the weather info from that place
 function getWeatherInfo(latitude, longitude) {
     console.log("Lat: " + latitude + "\nLon: " + longitude)
 
-    $.get("http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey+"&units=imperial"
-    ).done((response)=>{
+    $.get("http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial"
+    ).done((response) => {
         console.log(response)
+        //display results to page if successful
+        saveToHistory()
         displayResults(response)
-    }).fail(()=>{
+    }).fail(() => {
         alert("error")
     })
-    
+
 }
 
-function displayResults(data){
+//using data from weather api, display it on each card
+function displayResults(data) {
+    //show the main div
+    outputDivEl.removeAttr("hidden")
+    //display data on top card
     mainCardCity.text(searchCityName)
     var mainUnix = data.list[0].dt
-    console.log(mainUnix)
     var mainDate = convertUnixToDate(mainUnix)
-    console.log(mainDate)
     mainCardDate.text(mainDate)
+    mainCardIcon.attr("src", "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png")
+    mainCardTempSpan.text(data.list[0].main.temp + "°F")
+    mainCardWindSpan.text(data.list[0].wind.speed + "MPH")
+    mainCardHumiditySpan.text(data.list[0].main.humidity + "%")
 
-    console.log(cardDateEl)
+    //display data on other cards
+    //starting at 7th index and going up by 8 each time makes it so each entry is evenly spaced out by 24 hours
     var listIndex = 7;
-    for(var i = 0; i < cardDateEl.length; i++){
+    for (var i = 0; i < cardDateEl.length; i++) {
         var cardUnix = data.list[listIndex].dt
         var cardDate = convertUnixToDate(cardUnix)
         $(cardDateEl[i]).text(cardDate)
+        $(cardIconEl[i]).attr("src", "http://openweathermap.org/img/wn/" + data.list[listIndex].weather[0].icon + "@2x.png")
+        $(cardTempSpan[i]).text(data.list[listIndex].main.temp + "°F")
+        $(cardWindSPan[i]).text(data.list[listIndex].wind.speed + "MPH")
+        $(cardHumiditySpan[i]).text(data.list[listIndex].main.humidity + "%")
+
+
         listIndex += 8;
     }
 }
 
+//save search details to history and append them to the list
+function saveToHistory() {
+    //save necessary data to local storage
+    var dataToSave = {
+        term: searchTerm,
+        type: searchType,
+        name: searchCityName
+    }
+    var shouldSave = true
+    //go through and make sure there are no duplicates
+    for (var i = 0; i < searchHistoryList.length; i++) {
+        if (dataToSave.name == searchHistoryList[i].name) {
+            shouldSave = false;
+        }
+    }
+    if (shouldSave) {
+        searchHistoryList.push(dataToSave)
+        localStorage.setItem("history", JSON.stringify(searchHistoryList))
+    }
+    displayHistory()
+}
+
+//displays the history stored in local storage to the list below
+function displayHistory() {
+    //clear children from list
+    searchHistoryListEl.empty()
+
+    searchHistoryList = JSON.parse(localStorage.getItem("history"))
+
+    for (var i = 0; i < searchHistoryList.length; i++) {
+        var savedSearch = $('<a>')
+        //create a bootstrap list element
+        savedSearch.addClass("list-group-item list-group-item-action list-group-item-light history-item")
+        savedSearch.text(searchHistoryList[i].name)
+        savedSearch.attr("id", i)
+
+        searchHistoryListEl.append(savedSearch)
+    }
+    //reset event listeners for the list
+    setEventListeners()
+}
+
 //function to convert a unix time to a US date
-function convertUnixToDate(unix){
+function convertUnixToDate(unix) {
     var date = new Date(unix * 1000)
     date = date.toLocaleDateString("en-US")
     return date
 }
-//if it's valid and comes back with search results
-//save search to the search history section
-//save search to local storage
+//dont display search history if empty
+if (localStorage.getItem("history")) {
+    displayHistory()
+}
 
-//then use weather api info to populate big card, and the 5 small ones
+function setEventListeners(){
+$(".history-item").off()
+$(".history-item").on('click', function() {
+    var clickedId = parseInt($(this).attr("id"))
+    console.log(clickedId)
+
+    startSearchViaHistory(clickedId)
+})
+}
